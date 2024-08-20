dfcb905dce57f1d2a9538d75b15a1c711295752a
Robby6Strings <rausten93@gmail.com>
2024-08-20 13:08:51 +1200

    improve async fn type inference

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -2,7 +2,11 @@ import { SymbolResult } from "./polyfill.js"
 import type { ResultTuple } from "./polyfill"
 
 export function resultuple<const Fn extends (...args: any) => any>(fn: Fn) {
-  return function (...args: Parameters<Fn>): ResultTuple<ReturnType<Fn>> {
-    return fn[SymbolResult](...args)
+  return function (
+    ...args: Parameters<Fn>
+  ): ReturnType<Fn> extends Promise<infer T>
+    ? Promise<ResultTuple<T>>
+    : ResultTuple<ReturnType<Fn>> {
+    return fn[SymbolResult](...args) as any
   }
 }
--- sandbox/node/src/index.ts
+++ sandbox/node/src/index.ts
@@ -2,10 +2,44 @@ import { resultuple } from "resultuple"
 
 const safeAdd = resultuple((a: number, b: number) => {
   if (typeof a !== "number" || typeof b !== "number") {
-    throw new Error("Not a number")
+    throw new Error("cannot add - not a number")
   }
   return a + b
 })
 // @ts-ignore
-const [err, result] = safeAdd("1", 2)
-console.log(err, result)
+const [addErr, addRes] = safeAdd("test", 2)
+console.log(addErr, addRes)
+
+const [addErr2, addRes2] = safeAdd(1, 2)
+console.log(addErr2, addRes2)
+
+interface Product {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating: {
+    rate: number
+    count: number
+  }
+}
+
+const safeLoadProduct = resultuple(async (id: number) => {
+  if (typeof id !== "number") {
+    throw new Error("cannot load product - not a number")
+  }
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+  const product = await response.json()
+  return product as Product
+})
+
+;(async () => {
+  // @ts-ignore
+  const [productErr, productRes] = await safeLoadProduct("test")
+  console.log(productErr, productRes)
+
+  const [productErr2, productRes2] = await safeLoadProduct(1)
+  console.log(productErr2, productRes2)
+})()
